43e45488d415d1a3b9b545512a11611675a0fb59
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-07 11:56:14 +0530

    Add files via upload

---
+++ ThirdSmallestElementInArray.java
@@ -0,0 +1,40 @@
+//Program to find the Third Smallest Element in Array//
+
+package Recursion;
+import java.util.Scanner;
+
+public class ThirdSmallestElementInArray {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no . of elements to be entered in the array : ");
+		int n = sc.nextInt();
+		System.out.print("Enter the elements into the array : ");
+		int arr[] = new int[n];
+		for(int i = 0 ; i < n ; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Second Smallest value in the array is : "+ThirdSmallElement(arr , n));
+		sc.close();
+	}
+	public static int ThirdSmallElement(int arr[] , int n)
+	{
+		int temp ;
+		for (int i = 0; i < n ; i++)   
+        {  
+            for (int j = i + 1; j < n ; j++)   
+            {  
+                if (arr[i] > arr[j])   
+                {  
+                    temp = arr[i];  
+                    arr[i] = arr[j];  
+                    arr[j] = temp;  
+                }  
+            }  
+        }  
+       return arr[2];  
+	}
+
+}
