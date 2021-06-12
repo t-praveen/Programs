e07fd7674e87da370b82f19f03941f82db7ba314
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-12 16:00:29 +0530

    Add files via upload

---
+++ MegaBytesConverter.java
@@ -0,0 +1,26 @@
+package Recursion;
+import java.util.Scanner ; 
+
+public class MegaBytesConverter {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter KiloBytes : ") ;
+		int KiloBytes = sc.nextInt();
+		printMegaBytesAndKiloBytes(KiloBytes);
+		sc.close();
+	}
+	public static void printMegaBytesAndKiloBytes(int KiloBytes)
+	{
+		if( KiloBytes < 0 )
+			System.out.println("Invalid Value");
+		else
+		{
+			 int megabytes = ( KiloBytes / 1024 ) ;
+	         int kiloRemainder = ( KiloBytes % 1024 ) ;
+	         System.out.println(KiloBytes+" KB = "+megabytes+" MB and "+kiloRemainder+" KB");
+		}
+	}
+
+}
